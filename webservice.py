60f1aeb9fd22dd399659fa946b71a7028298dede
Sarah Medeiros <sarahpmedeiros@gmail.com>
2017-05-06 22:54:05 -0400

    Update webservice.py

--- webservice.py
+++ webservice.py
@@ -1,76 +1,79 @@
 import jsonschema
-from flask import Flask, jsonify, abort, make_response, request
+from flask import Flask, jsonify, abort, make_response, request, redirect
 from flask.ext.httpauth import HTTPBasicAuth
 from flask import render_template
 from werkzeug.utils import secure_filename
 from findPotentialStops import findPotentialStops
 from studentsGraph import studentsGraph
+import geocoder
 
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 students = [
 ]
+stops = [
+]
 app.config['MONGO_DBNAME'] = 'repo'
 app.config['MONGO_USERNAME'] = 'skaram13_smedeiro'
 app.config['MONGO_PASSWORD'] = 'skaram13_smedeiro'
 
-# @app.route('/app/api/v0.1/users/', methods=['GET'])
-# def get_user(user_id):
-#     user = [user for user in users if user['id'] == user_id]
-#     if len(user) == 0:
-#         abort(404)
-#     return jsonify({'user': user[0]})
-
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({'error': 'Not found foo.'}), 404)
+	return make_response(jsonify({'error': 'Not found foo.'}), 404)
 
 @app.route('/')
 def index():
-    return 'Index Page'
+	return 'Index Page'
 
-@app.route('/home')
-def home(name=None):
-    return render_template('home.html')
+@app.route('/home', methods=['POST','GET'])
+def newStudent():
+	if request.method == 'POST':
+		try:
+			print ('hi')
 
-@app.route('/app/api/v0.1/newStudent', methods=['POST','GET'])
-def create_student():
-    # print('here')
-    # print(request.json)
-    #TODO - these two files/functions
-    #findPotentialStops.execute(school="Everett ES", lon= -71.06918, lat= 42.29890)
-    #studentsGraph.execute(schoolInput="Everett ES", lonInput=-71.06918,  latInput= 42.29890)
+			address = request.form['Address']
+			# address = address.replace(' ','+')
+			city = request.form['City']
+			# city = city.replace(' ','+')
+			school = request.form['school']
+			print(address,city,school)
 
-    if not request.json:
-        print('Request not valid JSON.')
-        abort(400)
-    try:
-        # print ('hi')
-        # jsonschema.validate(request.json, schema)
-        print(request.json['lat'])
-        print(request.json['lng'])
-        print(request.json['school'])
-        # findPotentialStops.execute(school="Everett ES", lon= -71.06918, lat= 42.29890)
-        # print
-        # studentsGraph.execute(schoolInput="Everett ES", lonInput=-71.06918,  latInput= 42.29890)
-        # findPotentialStops.execute()
-        # findPotentialStops.execute(school=request.json['school'], lon= request.json['lng'], lat= request.json['lat'])
-        # print (temp)
-        # print (studentsGraph.execute(schoolInput=request.json['school'], lonInput=request.json['lng'],  latInput= request.json['lng']))
+			print (address +', ' + city +', MA')
+			g = geocoder.google(address +', ' + city +', MA')
+			print (g.latlng)
+			lat = g.latlng[0]
+			lng = g.latlng[1]
+			
+			
+			findPotentialStops.execute(schoolName=school, lon= lng, lat= lat)
+			print ('done')
+			# students.append(request.json)
+			# print(students)
 
-        students.append(request.json)
-        print(students)        
-        return jsonify(request.json), 201
-    except:
-        print('Request does not follow schema.')
-        abort(400)
+			temp = studentsGraph.execute(schoolInput=school, lonInput=lng,  latInput= lat)
+			
+			print (len(temp))
+			if (len(temp) > 0):
+				coords = temp[0]
+				print(coords)
+				values = lat, lng, coords['lat'], coords['lon']
+				return render_template('home.html',values=values)
+# 
+			return redirect('/')
+		except:
+			print('Request does not follow schema.')
+			abort(400)
+	else:
+		values = '42.3523891', '-71.1159753','42.3523891', '-71.1159753'
+		return render_template('home.html',values=values)
+
 
 
 @auth.error_handler
 def unauthorized():
-    return make_response(jsonify({'error': 'Unauthorized access.'}), 401)
+	return make_response(jsonify({'error': 'Unauthorized access.'}), 401)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
 
